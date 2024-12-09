a1e6a599e480601cac0ad07d20f03f525383e08d
Preeti <preetiyadav5443@gmail.com>
2024-12-09 15:08:43 +0530

    minor changes

--- server/api/webflow-form.post.js
+++ server/api/webflow-form.post.js
@@ -10,7 +10,7 @@ export default defineEventHandler(async (event) => {
         // Map Webflow form fields to ClickUp task fields
         const taskData = {
             name: `Form Submission: ${body.data["Name"]}`,
-            description: `**First Name**: ${body.data["Name"]} ${body.data["FirstName"]} ${body.data["LastName"]}\n**Last Name**: ${body.data["name"]}\n**Phone**: ${body.data["Phone"]}\n**Message**: ${body.data["Message"]}\n**Address**: ${body.data["address"]}\n**State**: ${body.data["state"]}`,
+            description: `**Name**: ${body.data["FirstName"]} ${body.data["LastName"]}\n**Phone**: ${body.data["Phone"]}\n**Message**: ${body.data["Message"]}\n**Address**: ${body.data["address"]}\n**State**: ${body.data["state"]}`,
             status: "to do",
             assignees: [5496465],
             priority: 1,
