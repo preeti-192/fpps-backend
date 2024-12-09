7f3659a934a6d16d0285127577958a83116b6fc3
Preeti <preetiyadav5443@gmail.com>
2024-12-09 11:07:05 +0530

    minor changes

--- server/api/webflow-form.post.js
+++ server/api/webflow-form.post.js
@@ -1,6 +1,10 @@
 import axios from "axios";
+const LIST_ID = process.env.CLICKUP_LIST_ID
+const API_TOKEN = process.env.CLICKUP_API_TOKEN
 
 export default defineEventHandler(async (event) => {
+    console.log(process.env.CLICKUP_LIST_ID, "CLICKUP_LIST_ID")
+
     try {
         // Parse the incoming JSON body
         const body = await readBody(event);
@@ -9,17 +13,17 @@ export default defineEventHandler(async (event) => {
         // Map Webflow form fields to ClickUp task fields
         const taskData = {
             name: `Form Submission: ${body.data["Name"]}`,
-            description: `**Email**: ${body.data["Email"]}\n**Phone**: ${body.data["Phone"]}\n**Message**: ${body.data["Message"]}`,
-            status: "to do",
+            description: `**Email**: ${body.data["Email"]}\n**Phone**: ${body.data["Phone"]}\n**Message**
+            : ${body.data["Message"]}`,
+            status: "open",
             assignees: [5496465],
             priority: 1,
             start_date: currentDate,
         };
 
         // ClickUp API details from environment variables
-        const listId = '901604956254';
-        const apiToken = 'pk_55289378_CMDSU7G0B4GOPHB83WVV92JOL73ELHDC';
-
+        const listId = LIST_ID;
+        const apiToken = API_TOKEN;
         // Send data to ClickUp
         await axios.post(
             `https://api.clickup.com/api/v2/list/${listId}/task`,
